eef8c43a688daf31177a8dcbcfe754b2a8113352
Luis Henriquez <weefouler@gmail.com>
2023-09-08 15:55:22 -0700

    tercero

--- src/lab8p2_luishenriquez/PAISES.java
+++ src/lab8p2_luishenriquez/PAISES.java
@@ -7,12 +7,24 @@ public class PAISES implements Serializable {
 
     private String nombre;
 
+    private ArrayList<Object> nadadores = new ArrayList();
+    
     private int medallas;
 
     public PAISES(String nombre, int medallas) {
         this.nombre = nombre;
         this.medallas = medallas;
     }
+
+    public ArrayList<Object> getNadadores() {
+        return nadadores;
+    }
+
+    public void setNadadores(ArrayList<Object> nadadores) {
+        this.nadadores = nadadores;
+    }
+    
+    
 
     public String getNombre() {
         return nombre;
